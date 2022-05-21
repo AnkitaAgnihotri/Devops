a1eb3554df0e5aadd05e1e366e3b7b0cfc9dbcd4
Ankita-Agnihotri <aankitaagnihotri@gmail.com>
2022-05-21 22:26:01 +0530

    Demo Project to set Infra for Continous deployment (Jenkins-Jfrog-Sonar-Apache)

---
+++ helloperl/hellomodified.pl
@@ -0,0 +1,9 @@
+#! /usr/bin/perl -w
+print("hii this is a perl program \n");
+print("this is git  \n");
+print("this is dec \n");
+print("poll scm\n");
+print("webhook trigger addded\n");
+print("recheck webhook \n");
+print("recheck 1\n");
+
